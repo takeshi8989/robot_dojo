c0203e5f2b8536bb817464b469af54135597de6e
takeshi8989 <htakeshi0614@gmail.com>
2025-01-05 22:37:13 -0800

    load model

--- genesis_humanoid_walk/train.py
+++ genesis_humanoid_walk/train.py
@@ -1,177 +1,60 @@
 import argparse
-import os
-import pickle
-import shutil
-
-from env import Go2Env
-from rsl_rl.runners import OnPolicyRunner
-
 import genesis as gs
 
 
-def get_train_cfg(exp_name, max_iterations):
-
-    train_cfg_dict = {
-        "algorithm": {
-            "clip_param": 0.2,
-            "desired_kl": 0.01,
-            "entropy_coef": 0.01,
-            "gamma": 0.99,
-            "lam": 0.95,
-            "learning_rate": 0.001,
-            "max_grad_norm": 1.0,
-            "num_learning_epochs": 5,
-            "num_mini_batches": 4,
-            "schedule": "adaptive",
-            "use_clipped_value_loss": True,
-            "value_loss_coef": 1.0,
-        },
-        "init_member_classes": {},
-        "policy": {
-            "activation": "elu",
-            "actor_hidden_dims": [512, 256, 128],
-            "critic_hidden_dims": [512, 256, 128],
-            "init_noise_std": 1.0,
-        },
-        "runner": {
-            "algorithm_class_name": "PPO",
-            "checkpoint": -1,
-            "experiment_name": exp_name,
-            "load_run": -1,
-            "log_interval": 1,
-            "max_iterations": max_iterations,
-            "num_steps_per_env": 24,
-            "policy_class_name": "ActorCritic",
-            "record_interval": -1,
-            "resume": False,
-            "resume_path": None,
-            "run_name": "",
-            "runner_class_name": "runner_class_name",
-            "save_interval": 100,
-        },
-        "runner_class_name": "OnPolicyRunner",
-        "seed": 1,
-    }
-
-    return train_cfg_dict
-
-
-def get_cfgs():
-    env_cfg = {
-        "num_actions": 12,
-        # joint/link names
-        "default_joint_angles": {  # [rad]
-            "FL_hip_joint": 0.0,
-            "FR_hip_joint": 0.0,
-            "RL_hip_joint": 0.0,
-            "RR_hip_joint": 0.0,
-            "FL_thigh_joint": 0.8,
-            "FR_thigh_joint": 0.8,
-            "RL_thigh_joint": 1.0,
-            "RR_thigh_joint": 1.0,
-            "FL_calf_joint": -1.5,
-            "FR_calf_joint": -1.5,
-            "RL_calf_joint": -1.5,
-            "RR_calf_joint": -1.5,
-        },
-        "dof_names": [
-            "FR_hip_joint",
-            "FR_thigh_joint",
-            "FR_calf_joint",
-            "FL_hip_joint",
-            "FL_thigh_joint",
-            "FL_calf_joint",
-            "RR_hip_joint",
-            "RR_thigh_joint",
-            "RR_calf_joint",
-            "RL_hip_joint",
-            "RL_thigh_joint",
-            "RL_calf_joint",
-        ],
-        # PD
-        "kp": 20.0,
-        "kd": 0.5,
-        # termination
-        "termination_if_roll_greater_than": 10,  # degree
-        "termination_if_pitch_greater_than": 10,
-        # base pose
-        "base_init_pos": [0.0, 0.0, 0.42],
-        "base_init_quat": [1.0, 0.0, 0.0, 0.0],
-        "episode_length_s": 20.0,
-        "resampling_time_s": 4.0,
-        "action_scale": 0.25,
-        "simulate_action_latency": True,
-        "clip_actions": 100.0,
-    }
-    obs_cfg = {
-        "num_obs": 45,
-        "obs_scales": {
-            "lin_vel": 2.0,
-            "ang_vel": 0.25,
-            "dof_pos": 1.0,
-            "dof_vel": 0.05,
-        },
-    }
-    reward_cfg = {
-        "tracking_sigma": 0.25,
-        "base_height_target": 0.3,
-        "feet_height_target": 0.075,
-        "reward_scales": {
-            "tracking_lin_vel": 1.0,
-            "tracking_ang_vel": 0.2,
-            "lin_vel_z": -1.0,
-            "base_height": -50.0,
-            "action_rate": -0.005,
-            "similar_to_default": -0.1,
-        },
-    }
-    command_cfg = {
-        "num_commands": 3,
-        "lin_vel_x_range": [0.5, 0.5],
-        "lin_vel_y_range": [0, 0],
-        "ang_vel_range": [0, 0],
-    }
-
-    return env_cfg, obs_cfg, reward_cfg, command_cfg
-
-
 def main():
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--exp_name", type=str, default="humanoid_walking")
-    parser.add_argument("-B", "--num_envs", type=int, default=4096)
-    parser.add_argument("--max_iterations", type=int, default=100)
+    parser.add_argument("-v", "--vis", action="store_true", default=False)
     args = parser.parse_args()
 
-    gs.init(logging_level="warning")
+    ########################## init ##########################
+    gs.init(backend=gs.cpu)
 
+    ########################## create a scene ##########################
+
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(),
+        viewer_options=gs.options.ViewerOptions(
+            camera_pos=(0, 0.0, 7),
+            camera_lookat=(0.3, -0.6, 0.5),
+            camera_fov=40,
+        ),
+        show_viewer=args.vis,
+        rigid_options=gs.options.RigidOptions(
+            dt=0.01
+        ),
+    )
+
+    ########################## entities ##########################
+    plane = scene.add_entity(gs.morphs.Plane())
+    import os
     current_dir = os.path.dirname(__file__)
-    log_dir = os.path.join(current_dir, f"logs/{args.exp_name}")
-    
-    env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
-    train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
-
-    if os.path.exists(log_dir):
-        shutil.rmtree(log_dir)
-    os.makedirs(log_dir, exist_ok=True)
-
-    env = Go2Env(
-        num_envs=args.num_envs, env_cfg=env_cfg, obs_cfg=obs_cfg, reward_cfg=reward_cfg, command_cfg=command_cfg
+    path = os.path.join(current_dir, "model/g1.xml")
+    robot = scene.add_entity(
+        gs.morphs.MJCF(file=path)
     )
 
-    runner = OnPolicyRunner(env, train_cfg, log_dir, device="mps")
+    ########################## build ##########################
+    scene.build()
 
-    pickle.dump(
-        [env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg],
-        open(f"{log_dir}/cfgs.pkl", "wb"),
-    )
+    gs.tools.run_in_another_thread(fn=run_sim, args=(scene, args.vis, robot))
+    if args.vis:
+        scene.viewer.start()
 
-    runner.learn(num_learning_iterations=args.max_iterations, init_at_random_ep_len=True)
+
+def run_sim(scene, enable_vis, robot):
+    i = 0
+    while True:
+        i += 1
+        scene.step()
+
+        if i > 200:
+            break
+
+    if enable_vis:
+        scene.viewer.stop()
 
 
 if __name__ == "__main__":
     main()
-
-"""
-# training
-python genesis_humanoid_walk/train.py -e humanoid_walking
-"""
